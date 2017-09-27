901e4ae6b7a623833c893f3e7fabf4116d245717
vinicius.souza <vinicius.souza@hightechcursos.com.br>
2017-09-26 21:52:26 -0400

    Criação do projeto Spring boot com conexão com banco

---
+++ src/main/java/br/com/fabricadeprogramador/Fabricaweb2Application.java
@@ -0,0 +1,12 @@
+package br.com.fabricadeprogramador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fabricaweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fabricaweb2Application.class, args);
+	}
+}
---
+++ src/main/java/br/com/fabricadeprogramador/model/Cliente.java
@@ -0,0 +1,105 @@
+package br.com.fabricadeprogramador.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String nome;
+	
+	@Column(nullable=false, length=50)
+	private String email;
+	
+	//Construtores
+	public Cliente() {}
+	
+	public Cliente(Long id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public Cliente(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+
+	//Getters and Setters
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	//toString
+	
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+	
+	//HashCode and Equals
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+}
