09e89a70ab8aeb32f4492b9661805c71ff91d665
vinicius.souza <vinicius.souza@hightechcursos.com.br>
2017-10-05 22:02:54 -0400

    Adaptando código para utilizar NamedQuery

--- src/main/java/br/com/fabricadeprogramador/model/Usuario.java
+++ src/main/java/br/com/fabricadeprogramador/model/Usuario.java
@@ -5,12 +5,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 
 import lombok.Data;
 
 @Data
 @Entity
+
+@NamedQueries(
+        {
+                @NamedQuery(name = "Usuario.buscarPorPerfil", 
+                		query = "select  new Usuario(u.id ,  u.nome, u.email)   from Usuario u where u.perfil.descricao= ?1")
+        }
+)
 public class Usuario {
 	
 	@Id
@@ -19,6 +28,7 @@ public class Usuario {
 	private Integer id;
 	
 	private String nome;
+	private String email;
 	private String senha;
 	
 	@JoinColumn
@@ -29,8 +39,9 @@ public class Usuario {
 	public Usuario() {
 	}
 
-	public Usuario(Integer id, String nome) {
+	public Usuario(Integer id, String nome, String email) {
 		this.id = id;
 		this.nome = nome;
+		this.email = email;
 	}
 }
--- src/main/java/br/com/fabricadeprogramador/repository/UsuarioRepository.java
+++ src/main/java/br/com/fabricadeprogramador/repository/UsuarioRepository.java
@@ -3,6 +3,7 @@ package br.com.fabricadeprogramador.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fabricadeprogramador.model.Usuario;
 
@@ -11,4 +12,11 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 	List<Usuario> findByNomeIgnoreCase(String nome);
 
     List<Usuario> findByNomeAndSenha(String nome, String senha);
+    
+    List<Usuario> findByPerfilDescricao(String descricao);
+
+    List<Usuario> buscarPorPerfil(String descricaoPerfil );
+    
+    @Query("select new Usuario(u.id,u.nome, u.email) from Usuario u where u.perfil.id=?1")
+    List<Usuario> findByPerfilId(Integer id);
 }
